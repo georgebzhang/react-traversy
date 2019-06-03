1cbaca47414df87fea54fd1f29f5ec202cb8fd8e
georgebzhang <georgebzhang5@gmail.com>
2019-06-03 09:46:38 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header'
 import Todos from './components/Todos';
 
 import './App.css';
@@ -36,13 +37,14 @@ class App extends Component {
 
   // Delete Todo
   delTodo = id => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+    // this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+    this.setState({ todos: this.state.todos.filter(todo => todo.id !== id) })
   }
 
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
