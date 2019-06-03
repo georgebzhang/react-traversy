7be2b16a2bf9b60cd7e1e5b2781cc6d6fce5deec
georgebzhang <georgebzhang5@gmail.com>
2019-06-02 23:37:15 -0700

    delete Todo

--- src/App.js
+++ src/App.js
@@ -34,11 +34,16 @@ class App extends Component {
     }) });
   }
 
+  // Delete Todo
+  delTodo = id => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>App</h1>
-        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
